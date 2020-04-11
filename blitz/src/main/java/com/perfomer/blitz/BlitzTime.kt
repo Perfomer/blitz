7d4959fa2a54488bb22bde0396456f7b3597d231
Denis Perfomer <perfomeria@gmail.com>
2020-04-11 13:29:39 +0700

    Update license

--- blitz/src/main/java/com/perfomer/blitz/BlitzTime.kt
+++ blitz/src/main/java/com/perfomer/blitz/BlitzTime.kt
@@ -3,6 +3,22 @@ package com.perfomer.blitz
 import androidx.annotation.PluralsRes
 import androidx.annotation.StringRes
 
+/*
+ * Designed and developed by 2020 Perfomer (Denis Balchugov)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 private const val SECOND_MILLIS = 1000L
 private const val MINUTE_MILLIS = 60L * SECOND_MILLIS
 private const val HOUR_MILLIS = 60L * MINUTE_MILLIS
